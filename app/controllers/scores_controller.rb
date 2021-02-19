0796053e3eef94c9ce3301ada7ca31d89a6ffafb
akitomoj <akitomoj@gmail.com>
2021-02-20 06:58:47 +0900

    words_indexpage_implement

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,6 @@
 class ScoresController < ApplicationController
+  
   def index
+    @week_day = ["日", "月", "火", "水", "木", "金", "土"][Time.new.wday]
   end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,11 @@
 class WordsController < ApplicationController
+
   def index
+    @word = Word.all
   end
+
+  def new
+    @word = Word.new
+  end
+  
 end
